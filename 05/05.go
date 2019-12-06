913ceec905db2caf9aec965fc6d9f58b3c3658b3
Mikael Jönsson <mikael.jonsson@fortnox.se>
2019-12-06 22:21:24 +0100

    Day 5, part 2

--- 05/05.go
+++ 05/05.go
@@ -26,7 +26,8 @@ func main() {
 	if scanner.Scan() {
 		line := scanner.Text()
 
-		fmt.Println(part1(toIntArray(strings.Split(line, ","))))
+		fmt.Println(part1(toIntArray(strings.Split(line, ",")), 1))
+		fmt.Println(part1(toIntArray(strings.Split(line, ",")), 5))
 		//fmt.Println(part2(toIntArray(strings.Split(line, ","))))
 	} else {
 		panic("oh no")
@@ -41,18 +42,18 @@ func value(codes []int, mode, val int) int {
 	return val
 }
 
-func part1(codes []int) int {
-	input := 1
+func part1(codes []int, input int) int {
 	output := 0
 
 	for i := 0; i <= len(codes); {
 		abcde := codes[i]
 		opcode := abcde % 100
+		mode1 := (abcde % 1000) / 100
+		mode2 := (abcde % 10000) / 1000
 
 		if opcode == 99 {
 			break
 		} else if opcode == 4 {
-			mode1 := (abcde % 1000) / 100
 			val1 := value(codes, mode1, codes[i + 1])
 			output = val1
 
@@ -61,22 +62,42 @@ func part1(codes []int) int {
 			codes[codes[i + 1]] = input
 			i += 2
 		} else if opcode == 1 {
-			mode1 := (abcde % 1000) / 100
-			mode2 := (abcde % 10000) / 1000
 			val1 := value(codes, mode1, codes[i + 1])
 			val2 := value(codes, mode2, codes[i + 2])
 			codes[codes[i + 3]] = val1 + val2
 
-
 			i += 4
 		} else if opcode == 2 {
+			val1 := value(codes, mode1, codes[i+1])
+			val2 := value(codes, mode2, codes[i+2])
+			codes[codes[i+3]] = val1 * val2
 
-			mode1 := (abcde % 1000) / 100
-			mode2 := (abcde % 10000) / 1000
-			val1 := value(codes, mode1, codes[i + 1])
-			val2 := value(codes, mode2, codes[i + 2])
-			codes[codes[i + 3]] = val1 * val2
-
+			i += 4
+		} else if opcode == 5 {
+			if value(codes, mode1, codes[i+1]) != 0 {
+				i = value(codes, mode2, codes[i+2])
+			} else {
+				i += 3
+			}
+		} else if opcode == 6 {
+			if value(codes, mode1, codes[i+1]) == 0 {
+				i = value(codes, mode2, codes[i+2])
+			} else {
+				i += 3
+			}
+		} else if opcode == 7 {
+			if value(codes, mode1, codes[i+1]) < value(codes, mode2, codes[i+2]) {
+				codes[codes[i + 3]] = 1
+			} else {
+				codes[codes[i + 3]] = 0
+			}
+			i += 4
+		} else if opcode == 8 {
+			if value(codes, mode1, codes[i+1]) == value(codes, mode2, codes[i+2]) {
+				codes[codes[i + 3]] = 1
+			} else {
+				codes[codes[i + 3]] = 0
+			}
 			i += 4
 		}
 	}
