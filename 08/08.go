7ba49b906cd1b341a365155ed9679c039d1ac608
Mikael Jönsson <mikael.jonsson@fortnox.se>
2019-12-08 22:44:45 +0100

    Day 8, part 2

--- 08/08.go
+++ 08/08.go
@@ -27,7 +27,7 @@ func digits(layer []int, digit int) int {
 	return cnt
 }
 
-func layerWithFewest(layers map[int][]int, digit int) (int, int) {
+func layerWithFewest(layers [][]int, digit int) (int, int) {
 	fewest := 0
 	fewestCnt := math.MaxInt32
 	for k, v := range layers {
@@ -42,20 +42,58 @@ func layerWithFewest(layers map[int][]int, digit int) (int, int) {
 }
 
 func part1(width, height int, array []int) int {
+	layers := layers(width, height, array)
+	layer, _ := layerWithFewest(layers, 0)
+	return digits(layers[layer], 1) * digits(layers[layer], 2)
+}
 
-	layers := make(map[int][]int)
+func part2(width, height int, array []int) {
+	layers := layers(width, height, array)
+
+	visibleLayer := make([]int, len(layers[0]))
+	for k := range visibleLayer {
+		visibleLayer[k] = 2
+	}
+
+	for _, v := range layers {
+		for lk, pixel := range v {
+			if visibleLayer[lk] == 2 {
+				visibleLayer[lk] = pixel
+			}
+		}
+	}
+
+	draw(width, visibleLayer)
+}
+
+func draw(width int, visibleLayer []int) {
+	for k, c := range visibleLayer {
+		if k % width == 0 {
+			fmt.Println()
+		}
+
+		if c == 0 {
+			fmt.Print(" ")
+		} else if c == 1 {
+			fmt.Print("█")
+		} else if c == 2 {
+			fmt.Print(" ")
+		}
+	}
+}
+
+func layers(width int, height int, array []int) [][]int {
+	layers := make([][]int, 0)
 	i := 0
 	for (i * width * height) < len(array) {
 		start := i * width * height
 		end := start + (width * height)
 		layer := array[start:end]
 
-		layers[i + 1] = layer
+		layers = append(layers, layer)
 		i++
 	}
-
-	layer, _ := layerWithFewest(layers, 0)
-	return digits(layers[layer], 1) * digits(layers[layer], 2)
+	return layers
 }
 
 func main() {
@@ -67,6 +105,7 @@ func main() {
 		line := scanner.Text()
 
 		fmt.Println(part1(25, 6, toIntArray(line)))
+		part2(25, 6, toIntArray(line))
 	} else {
 		panic("oh no")
 	}
